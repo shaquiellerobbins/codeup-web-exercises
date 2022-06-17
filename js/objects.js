882aceace4aee0b94e9106f54b035f7acd3d835c
shaquielle robbins <shaquiellerobbins@gmail.com>
2022-06-17 15:35:59 -0500

    completed objects exercise

--- js/objects.js
+++ js/objects.js
@@ -1,3 +1,5 @@
+
+
 (function() {
     "use strict";
 
@@ -11,10 +13,8 @@
      *  > console.log(person.firstName) // "Rick"
      *  > console.log(person.lastName) // "Sanchez"
      */
-
-
-
-
+    let person = {firstName:"Matthew", lastName:"Moreno"}
+    console.log(person);
     /**
      * TODO:
      * Add a sayHello method to the person object that returns a greeting using
@@ -24,7 +24,11 @@
      * Example
      * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
      */
-
+    person.sayHello = function() {
+        return ("Hello, " + this.firstName + " " + this.lastName + " how are you doing today.");
+    }
+    console.log(person.sayHello());
+    console.log("---")
     /** TODO:
      * HEB has an offer for the shoppers that buy products amounting to
      * more than $200. If a shopper spends more than $200, they get a 12%
@@ -39,11 +43,21 @@
      * and console.log the relevant messages for each person
      */
 
-    // var shoppers = [
-    //     {name: 'Cameron', amount: 180},
-    //     {name: 'Ryan', amount: 250},
-    //     {name: 'George', amount: 320}
-    // ];
+    var shoppers = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320}
+    ];
+    shoppers.forEach(function (shopper){
+        let discPrice = (shopper.amount - (shopper.amount * .12));
+        console.log("Hello " + shopper.name + " how are you.");
+        console.log("Your total came out to $" + shopper.amount);
+        if(shopper.amount > 200) {
+            console.log ("Congratulations " + shopper.name + ", you qualify for a discount of 12%! Your new total is $" + discPrice + "!");
+        }
+    })
+    console.log("---")
+    //     }
 
     /** TODO:
      * Create an array of objects that represent books and store it in a
@@ -57,7 +71,13 @@
      * > console.log(books[0].author.firstName) // "Douglas"
      * > console.log(books[0].author.lastName) // "Adams"
      */
-
+    var books = [
+        {title: "The Sorcerers Stone", author: {firstName: "J.K", lastName: "Rowling"}},
+        {title: "Eragon", author: {firstName: "Christopher", lastName: "Paolini"}},
+        {title: "The Life of Pi", author: {firstName: "Yann", lastName: "Martel"}},
+        {title: "The Book Thief", author: {firstName: "Markus", lastName: "Zusak"}},
+        {title: "Percy Jackson and The Lightning Thief", author: {firstName: "Rick", lastName: "Riordan"}}
+    ];
     /**
      * TODO:
      * Loop through the books array and output the following information about
@@ -82,7 +102,14 @@
      *      ---
      *      ...
      */
-
+    let i = 0
+    books.forEach(function (book) {
+        console.log("Book # " + (i + 1));
+        console.log("Title: " + book.title);
+        console.log("Author " + book.author.firstName + " " + book.author.lastName)
+        console.log("---")
+        i++
+    })
     /**
      * Bonus:
      * - Create a function named `createBook` that accepts a title and author
@@ -92,6 +119,19 @@
      * - Create a function named `showBookInfo` that accepts a book object and
      *   outputs the information described above. Refactor your loop to use your
      *   `showBookInfo` function.
+     *
      */
-
+    function createBook(titleName, authorName) {
+        let book;
+        book = [{title: titleName, author: authorName}];
+        return book;
+    }
+    function showBookInfo(bookObject) {
+        bookObject.forEach(function(info){
+            console.log("Book # " + (i + 1));
+            console.log("Title: " + info.title);
+            console.log("Author: " + info.author);
+        })
+    }
+    showBookInfo(createBook("Mocking Jay", "Suzanne Collins"))
 })();
